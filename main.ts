9ec84d751441c52206c4d67c372894b7b14de267
GusNeirinck <77126229+GusNeirinck@users.noreply.github.com>
2021-10-29 09:17:24 +0200

    ghost follow player, added shooting

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
-let mySprite: Sprite = null
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(assets.image`bullet`, Pete, 50, 50)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.coolRadial, 50)
+})
+let myEnemy: Sprite = null
+let projectile: Sprite = null
+let Pete: Sprite = null
 tiles.setTilemap(tilemap`bg`)
-let Pete = sprites.create(assets.image`Pete`, SpriteKind.Player)
+Pete = sprites.create(assets.image`Pete`, SpriteKind.Player)
 controller.moveSprite(Pete, 100, 100)
-game.onUpdateInterval(5000, function () {
-    mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+game.onUpdateInterval(100, function () {
+    myEnemy = sprites.create(assets.image`myImage`, SpriteKind.Enemy)
+    if (randint(0, 1) == 0) {
+        myEnemy.setPosition(6, randint(5, 110))
+    } else {
+        myEnemy.setPosition(150, randint(5, 110))
+    }
+    myEnemy.follow(Pete, 10)
 })
