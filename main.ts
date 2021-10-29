c7aba48d93eb4af3783b91c36da4cd384ad532e5
GusNeirinck <77126229+GusNeirinck@users.noreply.github.com>
2021-10-29 09:22:17 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(assets.image`bullet`, Pete, 50, 50)
 })
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    facingLeft = false
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    facingRight = true
+})
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy(effects.coolRadial, 50)
+    score += 1
 })
 let myEnemy: Sprite = null
+let facingRight = false
+let facingLeft = false
 let projectile: Sprite = null
 let Pete: Sprite = null
 tiles.setTilemap(tilemap`bg`)
+let score = 0
 Pete = sprites.create(assets.image`Pete`, SpriteKind.Player)
 controller.moveSprite(Pete, 100, 100)
 game.onUpdateInterval(100, function () {
